51792a36e323a8a42aa650240149db68f81992a9
Deepak Vijayagopalan <deepak.ramajayam@stu.ocsb.ca>
2026-09-16 21:11:55 +0000

    Added basicMath.cpp

---
+++ basicMath.cpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2026 Deepak V rights reserved
+// .
+// Created by: Deepak V
+// Date: Sep. 16, 2026
+// This program displays various math equations
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "6+6 = " << 6 + 6 << std::endl;  // addition
+    std::cout << "6-3 = " << 6 - 3 << std::endl;  // subtraction
+    std::cout << "6*6 = " << 6 * 6 << std::endl;  // multiplication
+    std::cout << "8/4 = " << 8 / 4 << std::endl;  // integer division
+    std::cout << "16/6 = " << 16 / 6 << std::endl;  // decimal division
+    std::cout << "12 % 5 = " << 12 % 5 << std::endl;  // modulo
+    std::cout << "5^2 = " << pow(5, 2) << std::endl;  // exponents
+    std::cout << "18^0.4 = " << pow(18, 0.4) << std::endl;  // exponents
+    std::cout << "√6 = " << sqrt(6) << std::endl;           // square root
+}
